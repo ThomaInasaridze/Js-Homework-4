d6d5f44c284fa8112d5b1fd78ffc27476ad28da6
ThomaInasaridze <115581820+ThomaInasaridze@users.noreply.github.com>
2022-12-21 21:36:19 +0400

    changes

--- js/task6.js
+++ js/task6.js
@@ -1,43 +1,20 @@
-// -----------------------------------------------------------------------
-
-
-
-const num=[];
-for (i=0;i<5;i++){
-    num.push(prompt("Enter a number"));
-}
-
-const result = false;
-for (i=0; i<5; i++){
-    if(num[i]>100){
-        result = true;
-
-    }
-}
-
-console.log(result);
-
-
-// --------------------------------------------------------
-
-
-
 const num1=[];
 for (i=0;i<5;i++){
     num1.push(prompt("Enter a number"));
 }
 
 
-const result1 = console.log("false");
+let result1 = false;
 for(let i=0; i<5;i++){
     if(num1[i]>=100){
 
-        console.log("true")
+        result1=true
 
     }
 }
+console.log(result1)
 
 
 //function
 
-console.log(num1.some(function (element) { if(element>100) {return true;} }));
+// console.log(num1.some(function (element) { if(element>100) {return true;} }));
